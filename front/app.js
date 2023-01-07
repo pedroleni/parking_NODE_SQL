022980e5f9cf72f6db9911c153b4ad2b04e9e23c
pedroleni <pedroleridanieto@hotmail.com>
2023-01-08 00:01:34 +0100

    comment

--- front/app.js
+++ front/app.js
@@ -1,8 +1,6 @@
+///// ------ FUNCION PARA LIMPIAR VISUALMENTE EL TABLERO NO AFECTA A LA BDO------
+
 const limpieza = () => {
-  let miarray = Array.from({ length: 15 }, () =>
-    Math.floor(Math.random() * 20)
-  );
-  console.log(miarray);
   fetch("http://localhost:8000/api/v1/parking/plazas")
     .then((res) => res.json())
     .then((data) => {
@@ -20,6 +18,8 @@ const extractId = (text) => {
 };
 
 limpieza();
+
+//// ------------ FUNCION PARA DESOCUPAR TODAS LAS PLAZAS DE LA BDO--------------
 
 const todoDesocupado = () => {
   let contador = 0;
@@ -60,6 +60,8 @@ const todoDesocupado = () => {
   }
 };
 
+///-----------MUESTRA LAS PLAZAS LIBRES ---------------------------
+
 const plazaLibres = () => {
   limpieza();
   fetch("http://localhost:8000/api/v1/parking/plazas")
@@ -75,6 +77,7 @@ const plazaLibres = () => {
     });
 };
 
+/// -------------MUESTRAS LAS PLAZAS TOTALES OCUPADAS -------------------------------
 const plazasOcupadasTotales = () => {
   limpieza();
   fetch("http://localhost:8000/api/v1/parking/plazas")
@@ -90,6 +93,7 @@ const plazasOcupadasTotales = () => {
     });
 };
 
+/// -------------------------MUESTRA PLAZAS PARES -----------------------------------------
 const plazasPares = () => {
   limpieza();
   fetch("http://localhost:8000/api/v1/parking/plazas")
@@ -107,6 +111,7 @@ const plazasPares = () => {
     });
 };
 
+/// -------------------------MUESTRA PLAZAS IMPARES -----------------------------------------
 const plazasImpares = () => {
   limpieza();
   fetch("http://localhost:8000/api/v1/parking/plazas")
@@ -123,6 +128,8 @@ const plazasImpares = () => {
       });
     });
 };
+
+/// -------------------------CON UN CLICK DESOCUPA PLAZA --------------------------------------
 
 const vaciarPlaza = (event) => {
   let padre = event.target.parentNode;
@@ -154,6 +161,8 @@ const vaciarPlaza = (event) => {
   }
 };
 
+/// -------------------------CON DOS CLICK OCUPA PLAZA --------------------------------------
+
 const ocuparPlaza = (event) => {
   const idCustom = extractId(event.target.id);
   const Edit = {
@@ -184,4 +193,141 @@ const ocuparPlaza = (event) => {
         event.target.appendChild(car);
       }
     });
+};
+
+/// -------------------------DESOCUPA PLAZA PARES  --------------------------------------
+const desocuparPlazasPares = () => {
+  let contador = 0;
+
+  while (contador < 21) {
+    const plazaActual = document.getElementById("plaza" + contador);
+    contador++;
+    if (contador % 2 == 0) {
+      const Edit = {
+        id: contador,
+        name: "plaza" + contador,
+        ocupada: false,
+      };
+      const formData = new FormData();
+      formData.append("json", JSON.stringify(Edit));
+      fetch("http://localhost:8000/api/v1/parking/plazas/" + contador, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Edit),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("desocupar", data);
+          if (plazaActual.hasChildNodes()) {
+            plazaActual.innerHTML = "";
+          }
+        });
+    }
+  }
+};
+
+//// ------------- RANDOM PLAZAS PARES -----------------------------------
+const plazasParesAleatorias = async () => {
+  let randomId = Array.from({ length: 15 }, () =>
+    Math.floor(Math.random() * 20)
+  );
+  console.log(randomId);
+
+  desocuparPlazasPares();
+
+  randomId.map((id) => {
+    const plazaActual = document.getElementById("plaza" + id);
+    if (id % 2 == 0) {
+      const Edit = {
+        id: id,
+        name: "plaza" + id,
+        ocupada: true,
+      };
+
+      const formData = new FormData();
+      formData.append("json", JSON.stringify(Edit));
+      fetch("http://localhost:8000/api/v1/parking/plazas/" + id, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Edit),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("ocupar", data);
+          plazasPares();
+        });
+    }
+  });
+};
+
+/// -------------------------DESOCUPA PLAZA IMPARES  --------------------------------------
+const desocuparPlazasImpares = () => {
+  let contador = 0;
+  while (contador < 21) {
+    const plazaActual = document.getElementById("plaza" + contador);
+    contador++;
+    if (contador % 2 !== 0) {
+      const Edit = {
+        id: contador,
+        name: "plaza" + contador,
+        ocupada: false,
+      };
+      const formData = new FormData();
+      formData.append("json", JSON.stringify(Edit));
+      fetch("http://localhost:8000/api/v1/parking/plazas/" + contador, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Edit),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("desocupar", data);
+          if (plazaActual.hasChildNodes()) {
+            plazaActual.innerHTML = "";
+          }
+        });
+    }
+  }
+};
+
+//// ------------- RANDOM PLAZAS IMPARES -----------------------------------
+const plazasImparesAleatorias = async () => {
+  let randomId = Array.from({ length: 15 }, () =>
+    Math.floor(Math.random() * 20)
+  );
+  console.log(randomId);
+
+  desocuparPlazasImpares();
+
+  randomId.map((id) => {
+    const plazaActual = document.getElementById("plaza" + id);
+    if (id % 2 !== 0) {
+      const Edit = {
+        id: id,
+        name: "plaza" + id,
+        ocupada: true,
+      };
+
+      const formData = new FormData();
+      formData.append("json", JSON.stringify(Edit));
+      fetch("http://localhost:8000/api/v1/parking/plazas/" + id, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Edit),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("ocupar", data);
+          plazasImpares();
+        });
+    }
+  });
 };
