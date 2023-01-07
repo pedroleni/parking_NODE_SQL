88a46d98ee3c95a7ef4a74123b69a9313e247b65
pedroleni <pedroleridanieto@hotmail.com>
2023-01-07 03:27:32 +0100

    routes change

--- src/API/parking/parking.routes.js
+++ src/API/parking/parking.routes.js
@@ -12,6 +12,6 @@ ParkingRoutes.get("/plazas", getAllPlazas);
 ParkingRoutes.get("/plazas/:id", getById);
 ParkingRoutes.delete("/plazas/:id", deleteById);
 ParkingRoutes.post("/plazas/:id", updateById);
-ParkingRoutes.post("/plazas/vaciar/:id", desocuparById);
+ParkingRoutes.post("/plazas/vacia/:id", desocuparById);
 
 module.exports = ParkingRoutes;
