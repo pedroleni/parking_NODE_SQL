036462c134d16112e83b0e6d1388931ab652047c
pedroleni <pedroleridanieto@hotmail.com>
2023-01-07 22:50:27 +0100

    css

--- front/app.js
+++ front/app.js
@@ -1,4 +1,8 @@
 const limpieza = () => {
+  let miarray = Array.from({ length: 15 }, () =>
+    Math.floor(Math.random() * 20)
+  );
+  console.log(miarray);
   fetch("http://localhost:8000/api/v1/parking/plazas")
     .then((res) => res.json())
     .then((data) => {
@@ -17,7 +21,61 @@ const extractId = (text) => {
 
 limpieza();
 
-const plaza = () => {
+const todoDesocupado = () => {
+  let contador = 0;
+
+  while (contador < 21) {
+    const plazaActual = document.getElementById("plaza" + contador);
+    contador++;
+    const Edit = {
+      id: contador,
+      name: "plaza" + contador,
+      ocupada: false,
+    };
+    const formData = new FormData();
+    formData.append("json", JSON.stringify(Edit));
+    fetch("http://localhost:8000/api/v1/parking/plazas/" + contador, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(Edit),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("desocupar", data);
+        if (plazaActual.hasChildNodes()) {
+          plazaActual.innerHTML = "";
+          const car = document.createElement("img");
+          car.src = "./img/free.jpg";
+          car.className = "imgFree";
+          plazaActual.appendChild(car);
+        } else {
+          const car = document.createElement("img");
+          car.src = "./img/free.jpg";
+          car.className = "imgFree";
+          plazaActual.appendChild(car);
+        }
+      });
+  }
+};
+
+const plazaLibres = () => {
+  limpieza();
+  fetch("http://localhost:8000/api/v1/parking/plazas")
+    .then((res) => res.json())
+    .then((data) => {
+      data.map((plaza) => {
+        const plazaActual = document.getElementById("plaza" + plaza.id);
+        const car = document.createElement("img");
+        car.src = "./img/free.jpg";
+        car.className = "imgFree";
+        if (plaza.ocupada == 0) plazaActual.appendChild(car);
+      });
+    });
+};
+
+const plazasOcupadasTotales = () => {
   limpieza();
   fetch("http://localhost:8000/api/v1/parking/plazas")
     .then((res) => res.json())
@@ -67,10 +125,10 @@ const plazasImpares = () => {
 };
 
 const vaciarPlaza = (event) => {
-  console.log("entro en vaciar");
-  const padre = event.target.parentNode;
-  if (padre.id !== "") {
+  let padre = event.target.parentNode;
+  if (padre.id !== "" && padre.id !== "container_parking") {
     const idCustom = extractId(padre.id);
+    console.log(padre.id);
     const Edit = {
       id: idCustom,
       name: "plaza" + idCustom,
@@ -94,4 +152,36 @@ const vaciarPlaza = (event) => {
         }
       });
   }
+};
+
+const ocuparPlaza = (event) => {
+  const idCustom = extractId(event.target.id);
+  const Edit = {
+    id: idCustom,
+    name: "plaza" + idCustom,
+    ocupada: true,
+  };
+
+  const formData = new FormData();
+  formData.append("json", JSON.stringify(Edit));
+  fetch("http://localhost:8000/api/v1/parking/plazas/" + idCustom, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(Edit),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("ocupar", data);
+      if (
+        !event.target.hasChildNodes() &&
+        event.target.className !== "imgCoche"
+      ) {
+        const car = document.createElement("img");
+        car.src = "./img/coche.jpg";
+        car.className = "imgCoche";
+        event.target.appendChild(car);
+      }
+    });
 };
