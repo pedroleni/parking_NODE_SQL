020fb88bef6aa1e924648c8d9fd7c417bcadee4f
pedroleni <pedroleridanieto@hotmail.com>
2023-01-07 20:14:49 +0100

    limpiar y plaza funcion

--- front/app.js
+++ front/app.js
@@ -1,7 +1,33 @@
-console.log("hols");
-
 fetch("http://localhost:8000/api/v1/parking/plazas/20")
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
   });
+
+const clear = () => {
+  fetch("http://localhost:8000/api/v1/parking/plazas")
+    .then((res) => res.json())
+    .then((data) => {
+      data.map((plaza) => {
+        const plazaActual = document.getElementById("plaza" + plaza.id);
+        if (plazaActual.hasChildNodes()) {
+          plazaActual.innerHTML = "";
+        }
+      });
+    });
+};
+
+const plaza = () => {
+  limpiar();
+  fetch("http://localhost:8000/api/v1/parking/plazas")
+    .then((res) => res.json())
+    .then((data) => {
+      data.map((plaza) => {
+        const plazaActual = document.getElementById("plaza" + plaza.id);
+        const car = document.createElement("img");
+        car.src = "./img/coche.jpg";
+        car.className = "imgCoche";
+        plazaActual.appendChild(car);
+      });
+    });
+};
